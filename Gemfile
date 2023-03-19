51deb0e048d43f2c662eb6396aef71ff33b08ab1
shima-atef <shimaatif731@gmail.com>
2023-03-19 08:52:43 +0200

    Add docs views and define actions

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 gem 'haml', '~> 6.1', '>= 6.1.1'
+gem"simple_form"
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root"welcome#index"
+  resources :docs
   # Defines the root path route ("/")
   # root "articles#index"
 end
