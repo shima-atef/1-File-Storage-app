2b3cc2168bea8442b08aab23f2d68cd99ab80d4e
shima-atef <shimaatif731@gmail.com>
2023-03-21 12:58:17 +0200

    Add Scss and stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  authenticated :user do
+    root "docs#index",as: "authenticated_root"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root"welcome#index"
   resources :docs
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
